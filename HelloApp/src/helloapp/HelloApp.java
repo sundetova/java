abd63500573b9673992dd6926f5d4e4e09032a07
sundetova <sun27aida@gmail.com>
2015-10-07 15:15:41 +0600

    Aida application

---
+++ HelloApp/src/helloapp/HelloApp.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloapp;
+import java.io.*;
+
+
+/**
+ *
+ * @author Aida
+ */
+public class HelloApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     /*  try
+        {
+        FileReader fr = new FileReader("c:\a.txt");
+       
+        catch (IOException e);*/
+        char[] Array={'A','i','d','a'};
+        
+        for(int i=0;i<Array.length;i++)
+        {
+        System.out.print(Array[i]);
+        }
+        System.out.println();
+    }
+    
+}
